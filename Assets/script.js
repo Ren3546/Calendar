aa5125e2bf6120a7d97413af0407117730b5b239
Rengo _ <renatovaldez6879@gmail.com>
2023-09-25 22:15:35 -0400

    added code to grab descriptions from local storage to place into corresponding time slot, code to display current date and time on the page

--- Assets/script.js
+++ Assets/script.js
@@ -9,7 +9,6 @@ $(document).ready(function(){
 //saves the description and the time slot to local storage
 localStorage.setItem(timeId, description)
   });
-});
 
 //loops through each time and compares is to the current time to apply the past, present, or future class. 
 for (var i=9; i < 18; i++) {
@@ -24,3 +23,16 @@ for (var i=9; i < 18; i++) {
       otherHour.classList.add('past')
     }
 }
+
+//loops through the user's description and saves each item in local storage to its corresponding time id (slot) on the page. 
+  for (var i=9; i < 18; i++) {
+   var userDesc = localStorage.getItem(i)
+   document.getElementById(i).children[1].value = userDesc
+  }
+  
+  //displays the current date and time on the page
+  var todaysDat = dayjs().format(`dddd, MMMM D, YYYY h:mm A`)
+  var headerDat = document.getElementById("currentDay")
+  
+  headerDat.textContent = todaysDat;
+});

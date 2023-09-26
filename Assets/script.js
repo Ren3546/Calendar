035907c2d6ba604fd5fdfbf5e64b3db4aa1ca166
Rengo _ <renatovaldez6879@gmail.com>
2023-09-25 22:13:25 -0400

    added user input description saves to local storage, if statements to add classes(colour) to time slots in relation to current time

--- Assets/script.js
+++ Assets/script.js
@@ -6,5 +6,21 @@ $(document).ready(function(){
     var timeId = this.parentElement.parentElement.id;
     var description = this.parentElement.parentElement.children[1].value;
    });
+//saves the description and the time slot to local storage
+localStorage.setItem(timeId, description)
   });
 });
+
+//loops through each time and compares is to the current time to apply the past, present, or future class. 
+for (var i=9; i < 18; i++) {
+  var otherHour = document.getElementById(i);
+  var currHour = dayjs().format(`[H]`)
+
+    if (otherHour.id == currHour) {
+      otherHour.classList.add('present')
+    } else if (otherHour.id >= currHour) {
+      otherHour.classList.add('future')
+    } else {
+      otherHour.classList.add('past')
+    }
+}
